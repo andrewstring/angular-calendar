74f18a6ba44cb77a531f5c72264741e0d0f12a8e
Andrew Stringfellow <abreezemail@gmail.com>
2024-07-13 23:11:01 -0400

    added day component for calendar...TODO: Fix scroll issue

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { DayComponent } from './day/day.component';
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [],
+  imports: [DayComponent],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss'
 })
---
+++ src/app/calendar/day/day.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-day',
+  standalone: true,
+  imports: [],
+  templateUrl: './day.component.html',
+  styleUrl: './day.component.scss'
+})
+export class DayComponent {
+
+  @Input() events?: any[];
+  @Input() day?: string;
+  @Input() number?: number;
+
+  constructor() {
+    this.events = [];
+    this.day = "MON";
+    this.number = 1;
+  }
+
+
+}
